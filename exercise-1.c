a6ca6711c3ef27c8dc4585b82c439f850783206c
aristides1000 <aristides1000@gmail.com>
2023-09-28 10:57:25 -0400

    feat: finish exercise 1

--- exercise-1.c
+++ exercise-1.c
@@ -5,8 +5,22 @@
 #include <stdio.h>
 
 int main() {
-    // Write C code here
-    printf("Hello world");
+  int i, numero, cont = 0;
 
-    return 0;
+  printf("Digite un numero: ");
+  scanf("%i", &numero);
+
+  for (i = 1; i <= numero; i++) {
+    if(numero % i == 0) {
+      cont++;
+    }
+  }
+
+  if (cont > 2) {
+    printf("El numero %i no es primo\n", numero);
+  } else {
+    printf("El numero %i es primo\n", numero);
+  }
+
+  return 0;
 }
